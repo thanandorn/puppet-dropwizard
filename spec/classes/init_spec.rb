a18424b7bbab53eea5bcc11c852525e4022bd3cb
Tron Thongsringklee <tron@bashton.com>
2015-07-21 21:00:50 +0100

    RSPEC: Add basic tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,24 @@
 require 'spec_helper'
-describe 'dropwizard' do
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('dropwizard') }
+describe 'dropwizard', :type => :class do
+  context 'supported operating systems' do
+    ['RedHat'].each do |osfamily|
+      ['CentOS'].each do |operatingsystem|
+        ['6','7'].each do |operatingsystemrelease|
+
+          describe "dropwizard class with no parameters on OS family #{operatingsystem}/#{osfamily}" do
+            let(:facts) {{
+              :operatingsystem => operatingsystem,
+              :osfamily        => osfamily,
+              :path            => '/bin:/sbin:/usr/bin:/usr/sbin',
+              :architecture    => 'x86_64'
+            }}
+
+            it { should contain_class('dropwizard') }
+
+          end
+        end
+      end
+    end
   end
 end
