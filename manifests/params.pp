c24a17cdc3f052fa4a6772a01e345a5515645897
Tron Thongsringklee <tron@bashton.com>
2016-01-21 10:01:27 +0000

    Config: Parameterise config file permission

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,6 +2,7 @@
 class dropwizard::params {
 
   $config_path           = '/etc/dropwizard'
+  $config_mode           = '0640'
   $sysconfig_path        = '/etc/sysconfig'
   $base_path             = '/opt'
   $run_user              = 'dropwizard'
